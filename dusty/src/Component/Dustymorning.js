2035318479ede490df4649bc8a1df5993ee45350
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-19 13:04:58 +0900

    Revert "Revert "Dusty_Project - morning.css""
    This reverts commit 11949967f54bc287b8c6654ca72c8741e7578c02.

--- dusty/src/Component/Dustymorning.js
+++ dusty/src/Component/Dustymorning.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Dustymorning.css';
 
 const Dustymorning = ({morning}) => {
     return (
-        <div>
+        <div className="d-morning">
             {morning}
         </div>
     );
